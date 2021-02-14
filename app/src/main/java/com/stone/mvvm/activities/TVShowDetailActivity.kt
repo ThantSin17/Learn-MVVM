7e78ac3a36c2c279078540051afb31f079b4d573
STONE <skybluestone7@gmail.com>
2021-02-14 23:54:50 +0630

    TVShowDetailActivity.kt

--- app/src/main/java/com/stone/mvvm/activities/TVShowDetailActivity.kt
+++ app/src/main/java/com/stone/mvvm/activities/TVShowDetailActivity.kt
@@ -2,15 +2,17 @@ package com.stone.mvvm.activities
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.stone.mvvm.R
@@ -18,6 +20,7 @@ import com.stone.mvvm.adapters.ImageSliderAdapter
 import com.stone.mvvm.databinding.ActivityTvShowDetailBinding
 import com.stone.mvvm.models.TVShow
 import com.stone.mvvm.viewModels.TVShowDetailsViewModel
+import java.util.*
 
 class TVShowDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTvShowDetailBinding
@@ -33,27 +36,60 @@ class TVShowDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_tv_show_detail)
         val tvShow= intent.getSerializableExtra("tvShow") as TVShow
+        binding.tvShow=tvShow
         toggleLoading()
         viewModel.getTVShowDetails(tvShow.id.toString()).observe(this,{ response ->
             if (response!=null){
 
                 toggleLoading()
                 loadImageSlider(response.tvShowDetails.pictures)
-//                Toast.makeText(this,response.tvShowDetails.toString(),Toast.LENGTH_SHORT).show()
+                binding.description = HtmlCompat.fromHtml(response.tvShowDetails.description,HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+                binding.rating=String.format(Locale.getDefault(),"%.2f",(response.tvShowDetails.rating).toDouble())
+                binding.genre=response.tvShowDetails.geners[0]
+                binding.runtime=response.tvShowDetails.runtime
+                binding.btnWebsite.setOnClickListener {
+                    val intent=Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse(response.tvShowDetails.url)
+                    startActivity(intent)
+                }
+
+//              Toast.makeText(this,response.tvShowDetails.toString(),Toast.LENGTH_SHORT).show()
             }
 
         })
+
+        binding.textReadMore.setOnClickListener {
+            if (binding.textReadMore.text.toString() == "Read More"){
+                binding.textDescription.maxLines= Int.MAX_VALUE
+                binding.textDescription.ellipsize=null
+                binding.textReadMore.text=getString(R.string.read_les)
+            }else{
+                binding.textDescription.maxLines= 4
+                binding.textDescription.ellipsize=TextUtils.TruncateAt.END
+                binding.textReadMore.text=getString(R.string.read_more)
+            }
+        }
+
+
+        binding.imageBack.setOnClickListener {
+            onBackPressed()
+        }
 
 
 
     }
 
+    private fun loadBinding() {
+      binding.layoutContainer.visibility=View.VISIBLE
+    }
+
     private fun loadImageSlider(sliderImages:Array<String>) {
         binding.sliderViewPager.offscreenPageLimit=1
         binding.sliderViewPager.adapter=ImageSliderAdapter(sliderImages)
-        binding.sliderViewPager.visibility=View.VISIBLE
-        binding.viewFadingEdge.visibility=View.VISIBLE
+
+
         setupSliderIndicator(sliderImages.size)
+        loadBinding()
         binding.sliderViewPager.registerOnPageChangeCallback(object :ViewPager2.OnPageChangeCallback(){
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
@@ -76,8 +112,10 @@ class TVShowDetailActivity : AppCompatActivity() {
             indicator[i]?.layoutParams=layoutParam
             binding.layoutIndicator.addView(indicator[i])
         }
-        binding.layoutIndicator.visibility=View.VISIBLE
+
         setCurrentSliderIndicator(0)
+
+
     }
     private fun setCurrentSliderIndicator(position:Int){
         val child=binding.layoutIndicator.childCount
